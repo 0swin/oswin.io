0f646eb80cfe298ebb5e6a7f45230784820384e8
Nicolas "0swin" Roger <nicolas@oswin.io>
2019-06-17 23:31:37 +0200

    Updated from Publii

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,3 @@ function toggleNav() {
 		$("nav").addClass("hidden")
 	}
 }
-
-document.getElementById('themeSwitch').addEventListener('change', function(event){
-  (event.target.checked) ? document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
-});
